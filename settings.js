5a54b4659cd2789bb268698874b15cef5cfa505c
lgndsmobile401 <96932371+lgndsmobile401@users.noreply.github.com>
2022-02-03 17:47:17 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ global.available = false //status online (online)
 
 // Other
 global.botname = "somebody"
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
+global.ownername= "somebody"
 global.myweb ="https://api-alphabot.herokuapp.com/"
 global.youtube = "https://youtube.com/c/ZeeoneOfc"
 global.github = "https://zeeoneofc.github.io/"
@@ -28,9 +28,9 @@ global.ownernomerr = "+62887435047326"
 global.thumbnail = "./image/lol.jpg"
 global.donasi = "./image/donasi.jpg"
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","6285342106390"] //ganti agar fitur owner bisa di gunakan
+global.owner = ["62881026720171","62881026720171","62881026720171"] //ganti agar fitur owner bisa di gunakan
 global.packname = 'kalo mau nyolong' //sticker wm
-global.author = 'izin dulu kontol' //sticker wm
+global.author = 'izin dulu' //sticker wm
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='

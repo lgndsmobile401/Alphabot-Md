83b52604760fdf1ecbc4f50d5701b14cc82fb15d
lgndsmobile401 <96932371+lgndsmobile401@users.noreply.github.com>
2022-02-06 10:40:03 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,8 +29,8 @@ global.thumbnail = "./image/lol.jpg"
 global.donasi = "./image/donasi.jpg"
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.owner = ["62881026720171","62881026720171","62881026720171"] //ganti agar fitur owner bisa di gunakan
-global.packname = 'oke' //sticker wm
-global.author = 'dek' //sticker wm
+global.packname = 'made in' //sticker wm
+global.author = 'Indonesia' //sticker wm
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
